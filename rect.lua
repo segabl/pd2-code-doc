900b18ad7cb5fdcb8e43add02d3e040c2a792b7b
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-03-20 17:38:10 +0100

    Document Rect

--- rect.lua
+++ rect.lua
@@ -1,11 +1,44 @@
+-- Rect is used as base class for all "drawable" UI elements here.
+-- It is unclear wether this applies to the actual game,
+-- but it's sufficient for this documentation to pass down functionality.
+
 ---@class Rect : UIElement
 Rect = {}
 
-function Rect:color(...) end
-function Rect:render_template(...) end
-function Rect:rotate(...) end
-function Rect:rotation(...) end
-function Rect:set_blend_mode(...) end
-function Rect:set_color(...) end
-function Rect:set_render_template(...) end
-function Rect:set_rotation(...) end
+---Returns the element's color.
+---@return Color
+function Rect:color() end
+
+---Sets the element's color.
+---@param color Color
+function Rect:set_color(color) end
+
+---Returns the element's blend mode.
+---@return '"add"'|'"mul"'|'"mulx2"'|'"normal"'|'"opacity_add"'|'"sub"'
+function Rect:blend_mode() end
+
+---Sets the element's blend mode.
+---@param mode '"add"'|'"mul"'|'"mulx2"'|'"normal"'|'"opacity_add"'|'"sub"'
+function Rect:set_blend_mode(mode) end
+
+---Returns the element's render template.  
+---The render template is used for advanced effects like background blur.
+---@return Idstring
+function Rect:render_template() end
+
+---Sets the element's render template.  
+---The render template is used for advanced effects like background blur.
+---@param template Idstring
+function Rect:set_render_template(template) end
+
+---Returns the element's rotation in degrees.
+---@return number
+function Rect:rotation() end
+
+---Sets the element's rotation in degrees.
+---@param angle number
+function Rect:set_rotation(angle) end
+
+---Rotates the element relative to its current rotation.
+---@param angle number
+function Rect:rotate(angle) end
