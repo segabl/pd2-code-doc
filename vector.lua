e56b6689e998b775cbc74e8fb4e1916b2fa17372
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-07-06 14:29:27 +0200

    Update vector.lua

--- vector.lua
+++ vector.lua
@@ -7,15 +7,15 @@
 ---@field z number
 Vector3 = {}
 
----Returns the cross product of the vector and `v`.
----@param v Vector3
+---Returns the cross product of the vector and `vec`.
+---@param vec Vector3
 ---@return Vector3
-function Vector3:cross(v) end
+function Vector3:cross(vec) end
 
----Returns the dot product of the vector and `v`.
----@param v Vector3
+---Returns the dot product of the vector and `vec`.
+---@param vec Vector3
 ---@return number
-function Vector3:dot(v) end
+function Vector3:dot(vec) end
 
 ---Returns the length of the vector.
 ---@return number
@@ -33,23 +33,49 @@ function Vector3:unpack() end
 ---@return Vector3
 function Vector3:with_x(x) end
 
----Returns a copy of the vector with its x coordinate set to `y`.
+---Returns a copy of the vector with its y coordinate set to `y`.
 ---@param y number
 ---@return Vector3
 function Vector3:with_y(y) end
 
----Returns a copy of the vector with its x coordinate set to `z`.
+---Returns a copy of the vector with its z coordinate set to `z`.
 ---@param z number
 ---@return Vector3
 function Vector3:with_z(z) end
 
-function Vector3:angle(...) end
-function Vector3:flat(...) end
-function Vector3:orthogonal_func(...) end
-function Vector3:orthogonal(...) end
-function Vector3:random_orthogonal(...) end
-function Vector3:rotate_HP(...) end
-function Vector3:rotate_with(...) end
-function Vector3:spread(...) end
+---Returns the smallest angle between the vector and `vec`.
+---@param vec Vector3
+---@return number
+function Vector3:angle(vec) end
+
+---@param vec Vector3
+---@return Vector3
+function Vector3:flat(vec) end
+
+---@param start_dir Vector3
+---@return function
+function Vector3:orthogonal_func(start_dir) end
+
+---@param ratio number
+---@return Vector3
+function Vector3:orthogonal(ratio) end
+
+---Returns a random vector that is orthogonal to the vector.
+---@return Vector3
+function Vector3:random_orthogonal() end
+
+---@param rot Rotation
+---@return Vector3
+function Vector3:rotate_HP(rot) end
+
+---@param rot Rotation
+---@return Vector3
+function Vector3:rotate_with(rot) end
+
+---@param spread number
+---@return Vector3
+function Vector3:spread(spread) end
+
+
 function Vector3:to_polar(...) end
 function Vector3:to_polar_with_reference(...) end
