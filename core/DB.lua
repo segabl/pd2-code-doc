4174c285a552775ba5f0bdc3d57f7eb1a45eecd3
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-07-26 15:22:58 +0200

    Update DB documentation

--- core/DB.lua
+++ core/DB.lua
@@ -6,23 +6,43 @@ DB = {}
 
 function DB:alive(...) end
 
-function DB:create_entry(...) end
+---Adds an asset to the database
+---@param type Idstring
+---@param id Idstring
+---@param path string
+function DB:create_entry(type, id, path) end
 
 function DB:extension(...) end
 
-function DB:has(...) end
+---Returns if the asset with the given id exists in the database
+---@param type string|Idstring
+---@param id string|Idstring
+---@return boolean
+function DB:has(type, id) end
 
-function DB:is_bundled(...) end
+---@return boolean
+function DB:is_bundled() end
 
 function DB:key(...) end
 
 function DB:load_node(...) end
 
-function DB:mods(...) end
+---Returns a table of all mod_overrides assets given by mod name and mod data
+---@return table<string, table>
+function DB:mods() end
 
-function DB:open(...) end
+---Opens an asset from the database for reading
+---@param type string|Idstring
+---@param id string|Idstring
+---@return file*
+function DB:open(type, id) end
 
-function DB:open_with_properties(...) end
+---Opens an asset from the database for reading with given properties
+---@param type string|Idstring
+---@param id string|Idstring
+---@param properties string[]
+---@return file*
+function DB:open_with_properties(type, id, properties) end
 
 function DB:reload_extra_overrides(...) end
 
@@ -34,4 +54,6 @@ function DB:set_extension(...) end
 
 function DB:set_mod_enabled(...) end
 
-function DB:tostring(...) end
+---Returns the string representation of the database
+---@return string
+function DB:tostring() end
