6e89c3123275bbf860c004d9ebb36a56690b9d5f
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-07-26 14:57:49 +0200

    Add SoundSource base

---
+++ core/SoundSource.lua
@@ -0,0 +1,74 @@
+---@meta
+
+---@class SoundSource
+SoundSource = {}
+
+function SoundSource:alive(...) end
+
+function SoundSource:audio_effect_bus(...) end
+
+function SoundSource:audio_effect_dry(...) end
+
+function SoundSource:default_listeners(...) end
+
+function SoundSource:delete(...) end
+
+function SoundSource:enable_env(...) end
+
+function SoundSource:env_enabled(...) end
+
+function SoundSource:extension(...) end
+
+function SoundSource:get_data(...) end
+
+function SoundSource:get_link(...) end
+
+function SoundSource:get_position(...) end
+
+function SoundSource:get_prefixes(...) end
+
+function SoundSource:get_switch(...) end
+
+function SoundSource:get_unit(...) end
+
+function SoundSource:hook(...) end
+
+function SoundSource:is_relative(...) end
+
+function SoundSource:key(...) end
+
+function SoundSource:link(...) end
+
+function SoundSource:link_occlusion(...) end
+
+function SoundSource:link_orientation(...) end
+
+function SoundSource:link_position(...) end
+
+function SoundSource:post_event(...) end
+
+function SoundSource:post_trigger(...) end
+
+function SoundSource:pre_hook(...) end
+
+function SoundSource:set_extension(...) end
+
+function SoundSource:set_link_object(...) end
+
+function SoundSource:set_listeners(...) end
+
+function SoundSource:set_occlusion(...) end
+
+function SoundSource:set_orientation(...) end
+
+function SoundSource:set_position(...) end
+
+function SoundSource:set_rtpc(...) end
+
+function SoundSource:set_switch(...) end
+
+function SoundSource:set_unit(...) end
+
+function SoundSource:stop(...) end
+
+function SoundSource:tostring(...) end
