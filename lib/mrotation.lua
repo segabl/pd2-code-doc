d0eff458ed6713b93ffa7ffc3a96f04f13c5df5a
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-10-17 11:33:34 +0200

    Add mvector3 and mrotation function documentation

---
+++ lib/mrotation.lua
@@ -0,0 +1,129 @@
+---@meta
+
+---@class mrotationlib
+mrotation = {}
+
+---Adds `r2` to `r1`
+---@param r1 Rotation
+---@param r2 Rotation
+function mrotation.add(r1, r2) end
+
+---Returns a copy of `r`
+---@param r Rotation
+---@return Rotation
+function mrotation.copy(r) end
+
+---Returns wether `r1` and `r2` are equal
+---@param r1 Rotation
+---@param r2 Rotation
+---@return boolean
+function mrotation.equal(r1, r2) end
+
+---Inverts `r`
+---@param r Rotation
+function mrotation.invert(r) end
+
+---Multiplies `r` by `mul`
+---@param r Rotation
+---@param mul number
+function mrotation.multiply(r, mul) end
+
+---Returns the pitch of `r`
+---@param r Rotation
+---@return number
+function mrotation.pitch(r) end
+
+---Returns the roll of `r`
+---@param r Rotation
+---@return number
+function mrotation.roll(r) end
+
+---Calculates the difference in rotation between `r2` and `r3` and stores the result in `r1`
+---@param r1 Rotation
+---@param r2 Rotation
+---@param r3 Rotation
+function mrotation.rotation_difference(r1, r2, r3) end
+
+---Sets `r` to the rotation representing `angle` on `axis`
+---@param r Rotation
+---@param axis Vector3
+---@param angle number
+function mrotation.set_axis_angle(r, axis, angle) end
+
+---Sets `r` to the rotation representing the look direction `dir` with up direction `up`
+---@param r Rotation
+---@param dir Vector3
+---@param up Vector3
+function mrotation.set_look_at(r, dir, up) end
+
+---Sets `r` to the rotation representing looking from `from` towards `to`
+---@param r Rotation
+---@param from Vector3
+---@param to Vector3
+function mrotation.set_look_from_to(r, from, to) end
+
+---Sets the pitch of `r`
+---@param r Rotation
+---@param pitch number
+function mrotation.set_pitch(r, pitch) end
+
+---Sets the roll of `r`
+---@param r Rotation
+---@param roll number
+function mrotation.set_roll(r, roll) end
+
+---Sets the vector components of `r`
+---@param r Rotation
+---@param x Vector3
+---@param y Vector3
+---@param z Vector3
+function mrotation.set_xyz(r, x, y, z) end
+
+---Sets yaw, pitch and roll of `r`
+---@param r Rotation
+---@param yaw number
+---@param pitch number
+---@param roll number
+function mrotation.set_yaw_pitch_roll(r, yaw, pitch, roll) end
+
+---Sets the yaw of `r`
+---@param r Rotation
+---@param yaw number
+function mrotation.set_yaw(r, yaw) end
+
+---Sets `r` to zero
+function mrotation.set_zero(r) end
+
+---Calculates the spherical linear interpolation between `r2` and `r3` and stores the result in `r1`
+---@param r1 Rotation
+---@param r2 Rotation
+---@param r3 Rotation
+---@param t number
+function mrotation.slerp(r1, r2, r3, t) end
+
+---Steps from `r2` to `r3` by `step` and stores the result in `r1`
+---@param r1 Rotation
+---@param r2 Rotation
+---@param r3 Rotation
+---@param step number
+function mrotation.step(r1, r2, r3, step) end
+
+---Sets `v` to the x component of `r`
+---@param r Rotation
+---@param v Vector3
+function mrotation.x(r, v) end
+
+---Sets `v` to the y component of `r`
+---@param r Rotation
+---@param v Vector3
+function mrotation.y(r, v) end
+
+---Returns the yaw of `r`
+---@param r Rotation
+---@return number
+function mrotation.yaw(r) end
+
+---Sets `v` to the z component of `r`
+---@param r Rotation
+---@param v Vector3
+function mrotation.z(r, v) end
---
+++ lib/mvector3.lua
@@ -0,0 +1,204 @@
+---@meta
+
+---@class mvector3lib
+mvector3 = {}
+
+---Scales `v2` to length `length` and adds it to `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+---@param length number
+function mvector3.add_scaled(v1, v2, length) end
+
+---Adds `v2` to `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+function mvector3.add(v1, v2) end
+
+---Returns the angle between `v1` and `v2`
+---@param v1 Vector3
+---@param v2 Vector3
+---@return number
+function mvector3.angle(v1, v2) end
+
+function mvector3.bezier(...) end
+
+---Returns a copy of `v`
+---@param v Vector3
+---@return Vector3
+function mvector3.copy(v) end
+
+---Calculates the cross product of `v2` and `v3` and stores the result in `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+---@param v3 Vector3
+function mvector3.cross(v1, v2, v3) end
+
+---Calculates the directional unit vector from `v2` to `v3`, stores the result in `v1` and returns the distance between `v2` and `v3`
+---@param v1 Vector3
+---@param v2 Vector3
+---@param v3 Vector3
+---@return number
+function mvector3.direction(v1, v2, v3) end
+
+---Returns the squared distance between `v1` and `v2`
+---@param v1 Vector3
+---@param v2 Vector3
+---@return number
+function mvector3.distance_sq(v1, v2) end
+
+---Returns the distance between `v1` and `v2`
+---@param v1 Vector3
+---@param v2 Vector3
+---@return number
+function mvector3.distance(v1, v2) end
+
+---Divides the components of `v` by `div`
+---@param v Vector3
+---@param div number
+function mvector3.divide(v, div) end
+
+---Returns the dot product of `v1` and `v2`
+---@param v1 Vector3
+---@param v2 Vector3
+---@return number
+function mvector3.dot(v1, v2) end
+
+---Returns wether `v1` and `v2` are equal
+---@param v1 Vector3
+---@param v2 Vector3
+---@return boolean
+function mvector3.equal(v1, v2) end
+
+function mvector3.get_index(...) end
+
+---Returns wether `v` is a zero vector
+---@param v Vector3
+---@return boolean
+function mvector3.is_zero(v) end
+
+---Returns the squared length of `v`
+---@param v Vector3
+---@return number
+function mvector3.length_sq(v) end
+
+---Returns the length of `v`
+---@param v Vector3
+---@return number
+function mvector3.length(v) end
+
+---Calculates the linear interpolation between `v2` and `v3` and stores the result in `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+---@param v3 Vector3
+---@param t number
+function mvector3.lerp(v1, v2, v3, t) end
+
+function mvector3.less(...) end
+
+function mvector3.max(...) end
+
+function mvector3.min(...) end
+
+---Multiplies the components of `v` by `mul`
+---@param v Vector3
+---@param mul number
+function mvector3.multiply(v, mul) end
+
+---Negates `v`
+---@param v Vector3
+function mvector3.negate(v) end
+
+---Normalizes `v`
+---@param v Vector3
+function mvector3.normalize(v) end
+
+---Returns wether `v1` and `v2` are not equal
+---@param v1 Vector3
+---@param v2 Vector3
+---@return boolean
+function mvector3.not_equal(v1, v2) end
+
+---Creates a random vector orthogonal to `v` and stores it in `v`
+---@param v Vector3
+function mvector3.random_orthogonal(v) end
+
+---Rotates `v` by `rot`
+---@param v Vector3
+---@param rot Rotation
+function mvector3.rotate_with(v, rot) end
+
+function mvector3.set_index(...) end
+
+---Scales the length of `v` to `l`
+---@param v Vector3
+---@param l number
+function mvector3.set_length(v, l) end
+
+---Sets the components of `v` to `x`, `y` and `z`
+---@param v Vector3
+---@param x number
+---@param y number
+---@param z number
+function mvector3.set_static(v, x, y, z) end
+
+---Sets the x component of `v` to `x`
+---@param v Vector3
+---@param x number
+function mvector3.set_x(v, x) end
+
+---Sets the y component of `v` to `y`
+---@param v Vector3
+---@param y number
+function mvector3.set_y(v, y) end
+
+---Sets the z component of `v` to `z`
+---@param v Vector3
+---@param z number
+function mvector3.set_z(v, z) end
+
+---Sets the components of `v` to zero
+---@param v Vector3
+function mvector3.set_zero(v) end
+
+---Sets `v1` to `v2`
+---@param v1 Vector3
+---@param v2 Vector3
+function mvector3.set(v1, v2) end
+
+---Randomly spreads `v` by `spread` degrees
+---@param v Vector3
+---@param spread number
+function mvector3.spread(v, spread) end
+
+---Steps from `v2` to `v3` by `step` and stores the result in `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+---@param v3 Vector3
+---@param step number
+function mvector3.step(v1, v2, v3, step) end
+
+---Scales `v2` to length `length` and subtracts it from `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+---@param length number
+function mvector3.subtract_scaled(v1, v2, length) end
+
+---Subtracts `v2` from `v1`
+---@param v1 Vector3
+---@param v2 Vector3
+function mvector3.subtract(v1, v2) end
+
+---Returns the x component of `v`
+---@param v Vector3
+---@return number
+function mvector3.x(v) end
+
+---Returns the y component of `v`
+---@param v Vector3
+---@return number
+function mvector3.y(v) end
+
+---Returns the z component of `v`
+---@param v Vector3
+---@return number
+function mvector3.z(v) end
