69959d4f3859230453056a6d613c328b9fc93c6e
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-03-01 16:47:21 +0100

    Add Global table

---
+++ Globals.lua
@@ -0,0 +1,28 @@
+---@meta
+
+Global = {}
+Global.game_settings = {}
+
+---@type boolean
+Global.editor_mode = nil
+
+---@type '"easy"'|'"normal"'|'"hard"'|'"overkill"'|'"overkill_145"'|'"easy_wish"'|'"overkill_290"'|'"sm_wish"'
+Global.game_settings.difficulty = nil
+
+---@type '"standard"'|'"crime_spree"'
+Global.game_settings.gamemode = nil
+
+---@type boolean
+Global.game_settings.one_down = nil
+
+---@type boolean
+Global.game_settings.single_player = nil
+
+---@type string
+Global.game_settings.mission = nil
+
+---@type string
+Global.game_settings.level_id = nil
+
+---@type integer
+Global.game_settings.reputation_permission = nil
