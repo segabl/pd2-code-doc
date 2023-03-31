50aefb662bf6a89595816ae4e529ce70b403f6d3
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-03-31 20:53:02 +0200

    Update Globals.lua

--- Globals.lua
+++ Globals.lua
@@ -6,14 +6,56 @@ Global.game_settings = {}
 ---@type boolean
 Global.editor_mode = nil
 
----@type '"easy"'|'"normal"'|'"hard"'|'"overkill"'|'"overkill_145"'|'"easy_wish"'|'"overkill_290"'|'"sm_wish"'
-Global.game_settings.difficulty = nil
+---@type boolean
+Global.game_settings.is_playing = nil
+
+---@type boolean
+Global.game_settings.auto_kick = nil
+
+---@type integer
+Global.game_settings.drop_in_option = nil
+
+---@type '"public"'|'"friends_only"'|'"private"'
+Global.game_settings.permission = nil
+
+---@type integer
+Global.game_settings.job_plan = nil
+
+---@type boolean
+Global.game_settings.search_modded_lobbies = nil
+
+---@type boolean
+Global.game_settings.search_appropriate_jobs = nil
 
 ---@type '"standard"'|'"crime_spree"'
 Global.game_settings.gamemode = nil
 
 ---@type boolean
+Global.game_settings.drop_in_allowed = nil
+
+---@type integer
+Global.game_settings.reputation_permission = nil
+
+---@type '"easy"'|'"normal"'|'"hard"'|'"overkill"'|'"overkill_145"'|'"easy_wish"'|'"overkill_290"'|'"sm_wish"'
+Global.game_settings.difficulty = nil
+
+---@type integer
+Global.game_settings.team_ai_option = nil
+
+---@type boolean
+Global.game_settings.team_ai = nil
+
+---@type integer
+Global.game_settings.kick_option = nil
+
+---@type boolean
 Global.game_settings.one_down = nil
+
+---@type boolean
+Global.game_settings.allow_modded_players = nil
+
+---@type boolean
+Global.game_settings.search_one_down_lobbies = nil
 
 ---@type boolean
 Global.game_settings.single_player = nil
@@ -23,6 +65,3 @@ Global.game_settings.mission = nil
 
 ---@type string
 Global.game_settings.level_id = nil
-
----@type integer
-Global.game_settings.reputation_permission = nil
