95db09e842f748b219eadc0fd0ed02579826c6c2
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-07-28 13:47:26 +0200

    Add Light

---
+++ src/diesel/classes/light.lua
@@ -0,0 +1,118 @@
+---@meta
+
+---@class Light: ScriptReference
+---@field type_name "Light"
+---@field protected type_id userdata
+---@field protected script_value boolean
+---@field protected script_reference boolean
+---@field private __property_writers table
+---@field private __properties table
+---@field private __tostring function
+---@field private __index table
+Light = {}
+
+function Light:ambient_cube_side(...) end
+
+function Light:children(...) end
+
+function Light:clipping_values(...) end
+
+function Light:color(...) end
+
+function Light:enable(...) end
+
+function Light:falloff_exponent(...) end
+
+function Light:far_range(...) end
+
+function Light:final_color(...) end
+
+function Light:inside_bs(...) end
+
+function Light:inside_bv(...) end
+
+function Light:inside_oobb(...) end
+
+function Light:link(...) end
+
+function Light:local_position(...) end
+
+function Light:local_rotation(...) end
+
+function Light:m_position(...) end
+
+function Light:m_rotation(...) end
+
+function Light:multiplier(...) end
+
+function Light:name(...) end
+
+function Light:near_range(...) end
+
+function Light:oobb(...) end
+
+function Light:parent(...) end
+
+function Light:position(...) end
+
+function Light:properties(...) end
+
+function Light:root(...) end
+
+function Light:rotation(...) end
+
+function Light:set_ambient_cube_side(...) end
+
+function Light:set_clipping_values(...) end
+
+function Light:set_color(...) end
+
+function Light:set_enable(...) end
+
+function Light:set_extension(...) end
+
+function Light:set_falloff_exponent(...) end
+
+function Light:set_far_range(...) end
+
+function Light:set_final_position(...) end
+
+function Light:set_local_position(...) end
+
+function Light:set_local_rotation(...) end
+
+function Light:set_multiplier(...) end
+
+function Light:set_near_range(...) end
+
+function Light:set_position(...) end
+
+function Light:set_projection_texture(...) end
+
+function Light:set_properties(...) end
+
+function Light:set_rotation(...) end
+
+function Light:set_specular_multiplier(...) end
+
+function Light:set_spot_angle_end(...) end
+
+function Light:set_spot_angle_start(...) end
+
+function Light:set_visibility(...) end
+
+function Light:specular_multiplier(...) end
+
+function Light:spot_angle_end(...) end
+
+function Light:spot_angle_start(...) end
+
+function Light:to_local(...) end
+
+function Light:to_world(...) end
+
+function Light:tostring(...) end
+
+function Light:unlink(...) end
+
+function Light:visibility(...) end
